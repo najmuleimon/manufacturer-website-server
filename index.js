b09d8ec89d7994c58159e9d5ad36928bf676e811
najmuleimon <najmul.eimon@gmail.com>
2022-05-22 20:16:38 +0600

    all products route added

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5tt4v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+        await client.connect();
+        const toolsCollection = client.db("manufacture").collection("products");
+
+        // get all tools
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+
+        // get a single tool
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tools = await toolsCollection.findOne(query);
+            res.send(tools);
+        })
+    } finally {
+
+    }
+
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from Manufacturer website');
