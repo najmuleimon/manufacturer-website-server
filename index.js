5ab9287750396eddeb69d4b679f72ffdca4a7f06
najmuleimon <najmul.eimon@gmail.com>
2022-05-24 22:21:33 +0600

    payment intent added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5tt4v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -36,6 +37,31 @@ async function run() {
         const toolsCollection = client.db("manufacture").collection("products");
         const orderCollection = client.db("manufacture").collection("orders");
         const userCollection = client.db("manufacture").collection("users");
+
+        // admin verification check
+        const verifyAdmin = async (req, res, next) => {
+            const initiator = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: initiator });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden!' });
+            }
+        }
+
+        // stripe payment intent
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const order = req.body;
+            const price = order.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
 
         // get all tools from tools collection
         app.get('/tools', async (req, res) => {
@@ -128,21 +154,14 @@ async function run() {
         })
 
         // put user role to make admin in user collection
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
-            const initiator = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: initiator });
-            if (requesterAccount.role === 'admin') {
-                const filter = { email: email };
-                const updateDoc = {
-                    $set: { role: 'admin' },
-                };
-                const result = await userCollection.updateOne(filter, updateDoc);
-                res.send(result);
-            }
-            else {
-                res.status(403).send({ message: 'Forbidden Access!' });
-            }
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
 
         })
 
